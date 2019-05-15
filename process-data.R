760217c2b8fd1e71d31dbe211a3826e8518c103a
Bridhett Puerto-Ulloa <bridhett17@yahoo.com>
2019-05-14 19:25:13 -0700

    inital commit

--- process-data.R
+++ process-data.R
@@ -4,13 +4,14 @@
 ### Your final product should contain the code along the following lines:
 ### the main part of it should be a function (e.g. 'repTable') that takes
 ### address as the argument, and returns a markdown table of representatives.
-
+library("httr")
+library("jsonlite")
+library("dplyr")
 ##    ---------- Google Civic Platform ----------
 ## 1. create the google civic platform request and httr::GET() the result
 ##    you need to include your api key in the request.  See the documentation
 ##    https://developers.google.com/civic-information/
 ##    in particular the reference section.
-##
 ##    There is also console where you can experiment with requests and see what do
 ##    these return.
 ##
@@ -18,6 +19,30 @@
 ##    httr::GET gives you an error, you may always put the address in your browser's
 ##    address bar.  If correct, it will display the corresponding JSON data.  If
 ##    incorrect, you get an error message.
+## getReps <- function(address) {
+  key <- readLines("~/Documents/Keys/google-key.txt")[1]
+  base <- "https://www.googleapis.com/civicinfo/v2/representatives"
+  address <- "22614 129th PL SE Kent, WA 98031"
+  
+  
+  query <- list(address=address, key=key)
+  res <- httr::GET(base, query=query)
+  httr::content(res)
+  
+  data <- content(res, "text") %>%
+    fromJSON()
+  
+  df1 <- data.frame(data$offices) %>%
+    select(name, officialIndices)
+    
+  vector <- list(df1$officialIndices) 
+  
+  
+  finalTable <- data[["officials"]] %>%
+    select(-address, -channels)
+    
+## }
+
 
 ## 2. extract the elected officials' data from the result
 ##    The data contains many relevant variables, including normalized address,
